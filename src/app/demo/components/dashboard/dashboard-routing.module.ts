e3d68b47fb3e309bdf7e386c9854a5f9c82275ee
Sajal Giri <srsajalgiridesk@gmail.com>
2024-07-23 11:12:50 +0530

    dashboard updated

--- src/app/demo/components/dashboard/dashboard-routing.module.ts
+++ src/app/demo/components/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import {CardModule} from 'primeng/card';
 
 @NgModule({
     imports: [RouterModule.forChild([
         { path: '', component: DashboardComponent }
     ])],
-    exports: [RouterModule]
+    exports: [RouterModule,CardModule]
 })
 export class DashboardsRoutingModule { }
