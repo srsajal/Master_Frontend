bdf35815f777de27b4b214bb854e7659fede2334
Sajal Giri <srsajalgiridesk@gmail.com>
2024-07-23 15:48:45 +0530

    dashboard updated

--- src/app/demo/components/dashboard/dashboard.component.ts
+++ src/app/demo/components/dashboard/dashboard.component.ts
@@ -13,27 +13,21 @@ import { SchemeHeadServiceService } from '../../service/MasterService/master-sch
 export class DashboardComponent implements OnInit {
     isActive: boolean = true;
 
-    totalDdo: number = 0;
     totalActiveDdo: number = 0;
     totalInActiveDdo: number = 0;
 
-    totalDetailHead: number = 0;
     totalActiveDetailHead: number = 0;
     totalInActiveDetailHead: number = 0;
 
-    totalSubDetailHead: number = 0;
     totalActiveSubDetailHead: number = 0;
     totalInActiveSubDetailHead: number = 0;
 
-    totalDepartment: number = 0;
     totalActiveDepartment: number = 0;
     totalInActiveDepartment: number = 0;
 
-    totalMajorHead: number = 0;
     totalActiveMajorHead: number = 0;
     totalInActiveMajorHead: number = 0;
 
-    totalSchemeHead: number = 0;
     totalActiveSchemeHead: number = 0;
     totalInActiveSchemeHead: number = 0;
 
@@ -54,64 +48,94 @@ export class DashboardComponent implements OnInit {
         };
         this.getActiveandInActiveCount(true);
         this.getActiveandInActiveCount(false);
-        this.totalDdo = this.totalActiveDdo + this.totalInActiveDdo
-        this.totalDetailHead = this.totalActiveDetailHead + this.totalInActiveDetailHead
-        this.totalSubDetailHead = this.totalActiveSubDetailHead + this.totalInActiveSubDetailHead
-        this.totalDepartment = this.totalActiveDepartment + this.totalInActiveDepartment
-        this.totalMajorHead = this.totalActiveMajorHead + this.totalInActiveMajorHead
-        this.totalSchemeHead = this.totalActiveSchemeHead + this.totalInActiveSchemeHead
 
     }
     getActiveandInActiveCount(isActive: boolean) {
-        if (isActive == true) {
-            this.totalActiveDdo = this.getMasterDdoCount(isActive);
-            this.totalActiveDetailHead = this.getMasterDetailHeadCount(isActive);
-            this.totalActiveSubDetailHead = this.getMasterSubDetailHeadCount(isActive);
-            this.totalActiveDepartment = this.getMasterDepartmentCount(isActive);
-            this.totalActiveMajorHead = this.getMasterMajorHeadCount(isActive);
-            this.totalActiveSchemeHead = this.getMasterSchemeHeadCount(isActive);
-        }
-        else if (isActive == false) {
-            this.totalInActiveDetailHead = this.getMasterDetailHeadCount(isActive);
-            this.totalInActiveDdo = this.getMasterDdoCount(isActive);
-            this.totalInActiveSubDetailHead = this.getMasterSubDetailHeadCount(isActive);
-            this.totalInActiveDepartment = this.getMasterDepartmentCount(isActive);
-            this.totalInActiveMajorHead = this.getMasterMajorHeadCount(isActive);
-            this.totalInActiveSchemeHead = this.getMasterSchemeHeadCount(isActive);
-        }
+        this.getMasterDdoCount(isActive);
+        this.getMasterDetailHeadCount(isActive);
+        this.getMasterSubDetailHeadCount(isActive);
+        this.getMasterDepartmentCount(isActive);
+        this.getMasterMajorHeadCount(isActive);
+        this.getMasterSchemeHeadCount(isActive);
     }
 
 
     // masterDdoService = inject(MasterService);
-    getMasterDdoCount(isActive: boolean): any {
-        this.masterDdoService.countMasterDdo(isActive, this.tableQueryParameters).subscribe((response: any) => {
-            return response;
-        });
+    getMasterDdoCount(isActive: boolean) {
+        if (isActive == true) {
+            this.masterDdoService.countMasterDdo(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalActiveDdo = response;
+            });
+        }
+        else if (isActive == false) {
+            this.masterDdoService.countMasterDdo(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalInActiveDdo = response;
+            });
+        }
+
     }
-    getMasterDetailHeadCount(isActive: boolean): any {
-        this.detailHeadService.countMasterDetailHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
-            return response;
-        });
+    getMasterDetailHeadCount(isActive: boolean) {
+        if (isActive == true) {
+            this.detailHeadService.countMasterDetailHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalActiveDetailHead = response;
+            });
+        }
+        else if (isActive == false) {
+            this.detailHeadService.countMasterDetailHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalInActiveDetailHead = response;
+            });
+        }
     }
-    getMasterSubDetailHeadCount(isActive: boolean): any {
-        this.subDetailHeadService.countMasterSubDetailHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
-            return response;
-        });
+    getMasterSubDetailHeadCount(isActive: boolean) {
+        if (isActive == true) {
+            this.subDetailHeadService.countMasterSubDetailHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalActiveSubDetailHead = response;
+            });
+        }
+        else if (isActive == false) {
+            this.subDetailHeadService.countMasterSubDetailHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalInActiveSubDetailHead = response;
+            });
+        }
     }
-    getMasterMajorHeadCount(isActive: boolean): any {
-        this.deparmentService.countMasterDepartment(isActive, this.tableQueryParameters).subscribe((response: any) => {
-            return response;
-        });
+    getMasterMajorHeadCount(isActive: boolean) {
+        if (isActive == true) {
+            this.deparmentService.countMasterDepartment(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalActiveMajorHead = response;
+            });
+        }
+        else if (isActive == false) {
+            this.deparmentService.countMasterDepartment(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalInActiveMajorHead = response;
+            });
+        }
+
     }
-    getMasterSchemeHeadCount(isActive: boolean): any {
-        this.schemeHeadService.countMasterSchemeHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
-            return response;
-        });
+    getMasterSchemeHeadCount(isActive: boolean) {
+        if (isActive == true) {
+            this.schemeHeadService.countMasterSchemeHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalActiveSchemeHead = response;
+            });
+        }
+        else if (isActive == false) {
+            this.schemeHeadService.countMasterSchemeHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalInActiveSchemeHead = response;
+            });
+        }
+
     }
-    getMasterDepartmentCount(isActive: boolean): any {
-        this.majorHeadService.countMasterMajorHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
-            return response;
-        });
+    getMasterDepartmentCount(isActive: boolean) {
+        if (isActive == true) {
+            this.majorHeadService.countMasterMajorHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalActiveDepartment = response;
+            });
+        }
+        else if (isActive == false) {
+            this.majorHeadService.countMasterMajorHead(isActive, this.tableQueryParameters).subscribe((response: any) => {
+                this.totalInActiveDepartment = response;
+            });
+        }
+
     }
 
 }
