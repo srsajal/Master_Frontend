52e68c87d8ea13cc42458e5d475089711ddacdbd
Somnath <somnath.ganguly.cse@gmail.com>
2024-07-25 11:27:25 +0530

    minor

--- src/app/demo/components/department/department.component.ts
+++ src/app/demo/components/department/department.component.ts
@@ -2,10 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActionButtonConfig, DynamicTable, DynamicTableQueryParameters } from 'mh-prime-dynamic-table';
-import { MessageService } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { IapiResponce } from 'src/Model/iapi-responce';
-import { Masterdept } from 'src/Model/master.model';
+import { Code, Masterdept } from 'src/Model/master.model';
 import { DepartmentServiceService } from '../../service/MasterService/department-service.service';
+import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { DepartmentformsComponent } from '../masterForms/departmentforms/departmentforms.component';
 
 
 @Component({
@@ -15,39 +17,65 @@ import { DepartmentServiceService } from '../../service/MasterService/department
 })
 export class DepartmentComponent implements OnInit {
 
-  istableLoading:boolean = false;
   tableData: any;
   tableQueryParameters!: DynamicTableQueryParameters | any;
   actionButtonConfig: ActionButtonConfig[] = [];
+  istableLoading: boolean = false;
   alldata: number = 0;
-  headertext = 'Add Department';
   visible: boolean = false;
   id: number = 0;
-  isSubUp: boolean = true;
+  codes: Code[] = [];
+  // headertext: string = 'ADD DDO DATA';
+  dialogButts: number = 1;
 
-  http = inject(HttpClient);
-  messageService = inject(MessageService)
-  constructor(private deptservice: DepartmentServiceService) { }
+  // http = inject(HttpClient);
+  messageService = inject(MessageService);
+  masterService = inject(DepartmentServiceService);
+  confirmationService = inject(ConfirmationService);
 
-  userForm: FormGroup = new FormGroup({
-    code: new FormControl('', [Validators.required, Validators.maxLength(2), Validators.minLength(2)]),
-    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    demandCode: new FormControl('', [Validators.required, Validators.minLength(1)]),
-    address: new FormControl('', [Validators.required]),
-    pinCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{6}')]),
-    phoneNumber: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
-    mobileNumber: new FormControl('', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
-    email: new FormControl('', [Validators.required, Validators.email])
-  });
+  ref: DynamicDialogRef | undefined;
+  constructor(public dialogService: DialogService, public config: DynamicDialogConfig) { }
+  show() {
+    this.ref = this.dialogService.open(DepartmentformsComponent, {
+      data: {
+        dialogButt: 1,
+        code: this.codes,
+        isDisable: false,
+        pgetData: this.getData.bind(this),
+
+      },
+      width: '50rem',
+      modal: true,
+      header: 'ADD TREASURY DATA'
+    });
+  }
+
+  // userForm: FormGroup = new FormGroup({
+  //   TreasuryCode: new FormControl('', [Validators.required, Validators.maxLength(3)]),
+  //   Code: new FormControl('', Validators.required),
+  //   Designation: new FormControl('', Validators.required),
+  //   Address: new FormControl(''),
+  //   Phone: new FormControl('', [Validators.required, Validators.maxLength(15)])
+  // });
 
   ngOnInit(): void {
+    // this.userForm.reset();
+    // this.userForm = this.initializeMasterForm();
+    this.tableInitialize();
+    this.getData();
+   // this.getCodeFromTreasury();
+    // console.log("table reloaded");
+
+  }
+
+  tableInitialize() {
     this.actionButtonConfig = [
-      // {
-      //   buttonIdentifier: 'view',
-      //   class: 'p-button-rounded p-button-raised',
-      //   icon: 'pi pi-eye',
-      //   lable: 'View',
-      // },
+      {
+        buttonIdentifier: 'view',
+        class: 'p-button-rounded p-button-raised',
+        icon: 'pi pi-eye',
+        lable: 'View',
+      },
       {
         buttonIdentifier: 'edit',
         class: 'p-button-warning p-button-rounded p-button-raised',
@@ -66,118 +94,166 @@ export class DepartmentComponent implements OnInit {
       pageIndex: 0,
       filterParameters: [],
     };
-    this.getData();
-    console.log(this.tableData);
   }
-
-  getData() {
-    this.deptservice.getMasterDepartment(true,this.tableQueryParameters).subscribe((response: any) => {
+  getData(isActive: boolean = true) {
+    // this.tableQueryParameters.filterParameters.push({
+    //   field: 'Id',
+    //   value: '1685',
+    //   operator:'equals'
+    // });
+    this.istableLoading = true;
+    this.masterService.getMasterDepartment(isActive, this.tableQueryParameters).subscribe((response: any) => {
+      this.istableLoading = false;
       this.tableData = response.result;
       this.alldata = response.result.dataCount;
-      console.log(this.tableData, response);
 
-
+      console.log(response);
     });
   }
+  // getCodeFromTreasury() {
+  //   this.masterService.getMasterCodeTreasury().subscribe((res: Code[]) => {
+  //     this.codes = res;
+  //     console.log(res);
 
-  submit(form: FormGroup) {
-    if (!this.userForm.invalid) {
-      console.log(this.userForm.value);
-      this.deptservice.postMasterDepartment(this.userForm).subscribe((res: Masterdept) => {
-        console.log(res);
-        this.getData();
-      });
-      form.reset();
-      this.visible = false;
-      this.messageService.add({ severity: 'success', summary: 'Confirmed', detail: 'Form Submitted', life: 2000 });
-    }
-    else {
-      this.messageService.add({ severity: 'warn', summary: 'warn', detail: 'Form  Invalid !!', life: 2000 });
-    }
-
-  }
+  //   },
+  //     error => {
+  //       console.error('Error fetching codes from Treasury:', error);
+  //       this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to fetch codes from Treasury', life: 2000 });
+  //     }
+  //   );
+  // }
 
   editData(tmpid: number) {
-    
-    this.deptservice.getMasterDepartmentById(tmpid).subscribe((res: Masterdept) => {
-      console.log(res);
-      this.userForm.patchValue({
-        code: res.code,
-        name: res.name,
-        demandCode: res.demandCode,
-        address: res.address,
-        pinCode: res.pinCode,
-        phoneNumber: res.phoneNumber,
-        mobileNumber: res.mobileNumber,
-        email: res.email
-      });
-      this.headertext = 'Edit Profile';
-      this.userForm.markAllAsTouched();
-      this.userForm.markAsDirty();
-    },
-      error => {
-        console.error('Error fetching department data by ID:', error);
-        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to fetch department data by ID', life: 2000 });
-      }
-    );
-    this.visible = true;
-    this.id = tmpid;
-    this.isSubUp = false;
-  }
-  cancel(form: FormGroup) {
-    this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have Cancelled', life: 2000 });
-    form.reset();
-    this.isSubUp = true;
-    this.visible = false;
-    this.headertext = 'Add Department';
-  }
-  hide(form: FormGroup) {
-    form.reset();
-    this.isSubUp = true;
-    this.visible = false;
-    this.headertext = 'Add Department';
-  }
+    this.ref = this.dialogService.open(DepartmentformsComponent, {
+      data: {
+        dialogButt: 2,
+        code: this.codes,
+        id: tmpid,
+        isDisable: false,
+        pgetData: this.showNormalData.bind(this),
 
-  update(form: FormGroup) {
-    if(this.userForm.valid){
-    this.deptservice.updateMasterDepartmentById(this.id, this.userForm).subscribe((res: any) => {
-      console.log(res);
-      this.getData();
+      },
+      width: '50rem',
+      modal: true,
+      header: 'EDIT TREASURY DATA'
     });
-    form.reset();
-    this.isSubUp = true;
-    this.visible = false;
-    this.messageService.add({ severity: 'success', summary: 'Confirmed', detail: 'Form Updated', life: 2000 });
   }
-  else{
-    this.messageService.add({ severity: 'warn', summary: 'Error', detail: 'Form Invalid', life: 2000 });
 
+  confirmDelete(id: number) {
+    this.confirmationService.confirm({
+      message: 'Do you want to delete this record?',
+      header: 'Delete Confirmation',
+      icon: 'pi pi-info-circle',
+      accept: () => {
+        this.delData(id);
+      },
+      reject: () => {
+        this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected' });
+      }
+    });
   }
-}
 
   delData(tmpid: number) {
-    const obj =confirm('are you sure want to delete');
-   if(obj){
-    this.deptservice.deleteMasterDepartmentById(tmpid).subscribe(() => {
-      this.getData();
+    this.masterService.deleteMasterDepartmentById(tmpid).subscribe(() => {
+      this.showNormalData();
       this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'Record deleted', life: 2000 });
     },
       error => {
-        console.error('Error deleting student data:', error);
-        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to delete record', life: 2000 });
+        console.error('Error deleting MasterDDO data:', error);
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to delete MasterDDO record', life: 2000 });
       }
     );
   }
-    
-  }
-  
-  
+  // restoreData(tmpid: number) {
+  //   this.masterService.restoreMasterTreasuryById(tmpid).subscribe(() => {
+  //     this.showDeletedData();
+  //     this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'Record restored', life: 2000 });
+  //   },
+  //     error => {
+  //       console.error('Error deleting MasterDDO data:', error);
+  //       this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to restore MasterDDO record', life: 2000 });
+  //     }
+  //   );
+  // }
 
-  showDialog() {
-    // console.log("showdialog called");
-    this.visible = true;
+
+
+
+
+  viewData(tmpid: number) {
+    this.masterService.getMasterDepartmentById(tmpid).subscribe((res: Masterdept) => {
+      this.ref = this.dialogService.open(DepartmentformsComponent, {
+        data: {
+          dialogButt: 3,
+          code: this.codes,
+          id: tmpid,
+          isDisable: true,
+          // pgetData : this.getData.bind(this),
+
+        },
+        width: '50rem',
+        modal: true,
+        header: 'EDIT TREASURY DATA'
+      });
+      //this.userForm.markAllAsTouched();
+      //this.userForm.markAsDirty();
+    },
+      error => {
+        console.error('Error fetching MasterDDO data by ID:', error);
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to fetch MasterDDO data by ID', life: 2000 });
+      }
+    );
   }
 
+
+  // showDialog() {
+  // console.log("showdialog called");
+  // this.visible = true;
+  // this.userForm.reset();
+  // this.userForm = this.initializeMasterForm(false);
+  // console.log(this.userForm);
+  // }
+
+  showDeletedData() {
+    this.actionButtonConfig = [
+      {
+        buttonIdentifier: 'view',
+        class: 'p-button-rounded p-button-raised',
+        icon: 'pi pi-eye',
+        lable: 'View',
+      },
+      {
+        buttonIdentifier: 'restore',
+        class: 'p-button-warning p-button-rounded p-button-raised',
+        icon: 'pi pi-undo',
+        lable: 'Restore',
+      },
+    ];
+    this.getData(false);
+  }
+  showNormalData() {
+    this.actionButtonConfig = [
+      {
+        buttonIdentifier: 'view',
+        class: 'p-button-rounded p-button-raised',
+        icon: 'pi pi-eye',
+        lable: 'View',
+      },
+      {
+        buttonIdentifier: 'edit',
+        class: 'p-button-warning p-button-rounded p-button-raised',
+        icon: 'pi pi-file-edit',
+        lable: 'Edit',
+      },
+      {
+        buttonIdentifier: 'del',
+        class: 'p-button-danger p-button-rounded p-button-raised',
+        icon: 'pi pi-trash',
+        lable: 'Delete',
+      }
+    ];
+    this.getData(true);
+  }
   handleRowSelection($event: any) {
     console.log("Download the details from above");
   }
@@ -186,11 +262,14 @@ export class DepartmentComponent implements OnInit {
       this.editData(event.rowData.id);
     }
     else if (event.buttonIdentifier == "del") {
-      this.delData(event.rowData.id);
+      this.confirmDelete(event.rowData.id);
     }
-    else {
-
+    else if (event.buttonIdentifier == "view") {
+      this.viewData(event.rowData.id);
     }
+    // else if (event.buttonIdentifier == "restore") {
+    //   this.restoreData(event.rowData.id);
+    // }
   }
   handQueryParameterChange(event: any) {
     this.tableQueryParameters = {
@@ -198,7 +277,21 @@ export class DepartmentComponent implements OnInit {
       pageIndex: event.pageIndex,
       filterParameters: event.filterParameters || [],
     };
+    console.log(this.tableQueryParameters)
     this.getData();
+  }
+  handleSearch(event: any) {
+    // this.tableQueryParameters.filterParameters = [];
+    // this.tableData.headers.forEach((element: { filterField: any; }) => {
+    //   this.tableQueryParameters.filterParameters.push({
+    //     field: element.filterField,
+    //     value: event,
+    //     operator: 'contains'
+    //   });
+    // });
+    console.log(event);
+    // console.log(this.tableQueryParameters);
+    // this.getData();
   }
 
 }
